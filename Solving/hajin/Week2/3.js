36ae96e5d4bb1d27c9237ed6ccbed20a379ea983
Hajin-Bang <hajin0619@naver.com>
2024-01-24 03:18:04 +0900

    전화번호 목록

--- Solving/hajin/Week2/3.js
+++ Solving/hajin/Week2/3.js
@@ -12,7 +12,7 @@ phone_book의 길이는 1 이상 1,000,000 이하입니다.
 같은 전화번호가 중복해서 들어있지 않습니다.
 */
 
-// 답
+// 답1
 function solution(phone_book) {
   const hash = {};
 
@@ -30,3 +30,38 @@ function solution(phone_book) {
   }
   return true;
 }
+
+// 답2
+function solution(phone_book) {
+  const phoneMap = new Map();
+
+  // 해시맵에 전화번호 저장
+  for (let phoneNumber of phone_book) {
+    phoneMap.set(phoneNumber, true);
+  }
+
+  // 각 전화번호가 다른 번호의 접두어인지 확인
+  for (let phoneNumber of phone_book) {
+    for (let i = 1; i < phoneNumber.length; i++) {
+      let prefix = phoneNumber.slice(0, i);
+      if (phoneMap.has(prefix)) {
+        return false;
+      }
+    }
+  }
+
+  // 모든 번호가 확인되었을 때는 중복된 번호가 없으므로 true 반환
+  return true;
+}
+
+// slice 와 substring
+// 둘 다 start 인덱스부터 end-1 인덱스까지 반환하는 것은 동일
+
+// 차이점은?
+// start 값이 end 값보다 작은 경우:
+// substring: 두 인덱스 값을 서로 교환해서 정상적으로 동작
+// slice: 빈 문자열(””)을 결과로 반환
+
+// start, end에 음수인 값을 전달할 경우:
+// substring: 음수를 0으로 변경해서 메소드를 수행
+// slice: 문자열의 마지막 인덱스를 기준으로 뺀 값을 사용해서 메소드를 정상적으로 수행
